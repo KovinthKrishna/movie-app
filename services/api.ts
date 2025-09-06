9f76f88180b1e4b9ee3c6598814a7ed1d892eada
Kovintharajan Krishnasuntharam <KovinthKrishna@gmail.com>
2025-09-06 18:20:32 +0530

    Implement MovieCard component

--- services/api.ts
+++ services/api.ts
@@ -1,9 +1,8 @@
 export const TMDB_CONFIG = {
   BASE_URL: "https://api.themoviedb.org/3",
-  API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
   headers: {
     accept: "application/json",
-    Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_READ_ACCESS_TOKEN}`,
+    Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`,
   },
 };
 
